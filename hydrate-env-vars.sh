4b720f2e363a0016b6426db01b28098a5d847efa
Simon Whitaker <simon@shiftlab.ai>
2022-06-11 21:44:37 +0100

    Use bash in hydrate script

--- hydrate-env-vars.sh
+++ hydrate-env-vars.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
 
 # Replace any instance of `__ENV_XXX__` in the source code with the value of the environment variable `XXX`
 perl -pi -e "s|__ENV_([A-Z_]+)__|\$ENV{\$1}|g" /app/dashboard/*
